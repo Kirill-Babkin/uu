0794bbf9b2e55182b0fe410a077f9dce586e4996
Kir <babkin.kir@gmail.com>
2024-06-14 21:27:58 -0400

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,16 +7,52 @@ import {
   UserButton
 } from '@clerk/nextjs'
 
+import {Inter, Space_Grotesk} from 'next/font/google'
+import { Metadata } from 'next'
+
 import './globals.css'
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter'
+})
+
+const SpaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700' ],
+  variable: '--font-spaceGrotesk'
+})
+
+
+export const metadata: Metadata = {
+  title: "Uplift Universe",
+  description: "Welcome to Uplift Universe! 🌟 Your daily dose of inspiration, motivation, and self-discovery.We're on a mission to illuminate your journey with uplifting stories, thought-provoking animations, and practical wisdom for personal growth. 🌈",
+  icons: {
+    icon: "/uu-logo.svg"
+  }
+}
+
+
+
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      elements: {
+        formButtonPrimary: 'primary-gradient',
+        footerActionLink:' primary-text-gradient hover:text-primary-500',
+      }
+    }
+    }>
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${SpaceGrotesk.variable}`}>
+
+          <h1 className='h1-bold'>This is some text</h1>
           <SignedOut>
             <SignInButton />
           </SignedOut>
